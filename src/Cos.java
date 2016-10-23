ac851494f22b75a37a447ce08d036a3b4318fa38
Krystian <krystian.skrzyszewski@gmail.com>
2016-10-23 12:05:18 +0200

    trzeci branch

--- src/Cos.java
+++ src/Cos.java
@@ -3,7 +3,7 @@ public class Cos {
 
 	public static void main(String[] args) {
 		System.out.println("Marco polo miał dziurawe gacie");
-		System.out.println("Żarcik");
+		System.out.println("A może jednak nie...");
 	}
 
 }
